ac5d430821213f882a1d2f1de17f81f57700c61e
Salvador Marquez <imt.simm@hotmail.com>
2019-06-07 16:13:10 -0500

    Base project

---
+++ accelerometerApp/AccelerometerApp/AccelerometerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccelerometerApp
+//
+//  Created by Salvador Marquez on 6/7/19.
+//  Copyright © 2019 Citsa Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
