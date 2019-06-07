48a64b48d223f86b2b12ba342ee2b43c52966ae4
Salvador Marquez <imt.simm@hotmail.com>
2019-06-07 17:09:18 -0500

    Basic app that retrives the accelerometer values and displays it

--- accelerometerApp/AccelerometerApp/AccelerometerApp/ViewController.swift
+++ accelerometerApp/AccelerometerApp/AccelerometerApp/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblX: UILabel!
+    @IBOutlet weak var lblY: UILabel!
+    @IBOutlet weak var lblZ: UILabel!
+    
+    var motionManager : CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+    }
+
+    func updateLabels(data: CMAccelerometerData?, error: Error?){
+        guard let accelerometerData = data else {return}
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 2
+        
+        let x = formatter.string(for: accelerometerData.acceleration.x)
+        let y = formatter.string(for:accelerometerData.acceleration.y)
+        let z = formatter.string(for: accelerometerData.acceleration.z)
+        
+        lblX.text = "X: \(x!)"
+        lblY.text = "Y: \(y!)"
+        lblZ.text = "Z: \(z!)"
+        
     }
 
 
